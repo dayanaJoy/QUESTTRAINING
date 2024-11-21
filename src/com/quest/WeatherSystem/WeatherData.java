7028a1ffbcbf4b2eaebecc75ca60199bd1c3dd2f
dayanaJoy <joydayana8@gmail.com>
2024-11-21 17:20:45 +0530

    Weather system

---
+++ src/com/quest/WeatherSystem/WeatherData.java
@@ -0,0 +1,49 @@
+package com.quest.WeatherSystem;
+
+public abstract class WeatherData {
+    String name;
+    double temp;
+    double humidity;
+    String condition;
+
+    public WeatherData(String name, double temp, double humidity, String condition) {
+        this.name = name;
+        this.temp = temp;
+        this.humidity = humidity;
+        this.condition = condition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public abstract void display() ;
+}
